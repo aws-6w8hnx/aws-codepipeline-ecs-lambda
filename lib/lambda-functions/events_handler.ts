6d1fbbd276f8c577626697dbf8a9d6b7df2bd876
Jacky Fan <jfan@FQs-MBP.local>
2024-06-28 14:19:28 +1200

    move async lambda code to assets dir

--- lib/lambda-functions/events_handler.ts
+++
@@ -1,7 +0,0 @@
-import { EventBridgeEvent, Context } from 'aws-lambda';
-
-export const handler = async (event: EventBridgeEvent<string, any>) => {
-    console.log('LogEvent');
-    console.log('Received event:', JSON.stringify(event, null, 2));
-    return 'Finished';
-};
